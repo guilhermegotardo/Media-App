49f9c7877424070260b465f2da6167ee7256f81a
Guilherme Gotardo <guilhermegotardo@outlook.com>
2025-04-29 21:22:27 -0300

    feat: added debug compile optionfor vs-code

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
-cmake -B build/
+DEBUG_MODE=$1
+ARG=""
+rm -rf build/* build/.*
+if [ "$1" = "d" ]; then
+    echo "Make DEBUG version"
+    cmake -Bbuild -DCMAKE_BUILD_TYPE=Debug
+    cmake --build build
+elif [ "$1" = "r" ]; then
+    echo "Make RELEASE version"
+    cmake -B build/
+else
+    echo "Unknown command: $1"
+    echo "Usage: $0 {d|r}"
+    exit 1
+fi
 
 cd build/
 make
